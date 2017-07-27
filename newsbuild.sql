2aa580734176aa341fb036bafb8ab511048a1f10
obleft <bhhakes@gmail.com>
2017-07-27 09:45:18 -0500

    refactor: updated uri_log for better efficiency

--- newsbuild.sql
+++ newsbuild.sql
@@ -3,17 +3,16 @@
 
 CREATE VIEW uri_log AS
 -- create view of article log only
-   SELECT split_part(path, '/article/', 2) as article_short, count(id) as visits
+   SELECT path, count(id) as visits
    FROM log
-   WHERE path like '/article/%'
-   GROUP BY article_short
+   GROUP BY path
    ORDER BY visits desc;
 
 CREATE VIEW art_rank AS
 -- create view of article rank by site visits
    SELECT articles.title, uri_log.visits as visits, articles.author as author_id
    FROM uri_log JOIN articles
-   ON articles.slug = uri_log.article_short
+   ON '/article/' || articles.slug = uri_log.path
    ORDER BY uri_log.visits desc;
 
 CREATE VIEW auth_rank AS
@@ -29,5 +28,4 @@ CREATE VIEW log_status_rank AS
    SELECT date(time) as day, count(status) as total, count(status) FILTER (WHERE status = '200 OK') as success, count(status) FILTER (WHERE status = '404 NOT FOUND') as err_code, (count(status) FILTER (WHERE status = '404 NOT FOUND') * 1.000) / (count(status) * 1.000) as percent_errors
    FROM log
    Group BY date(time)
-   ORDER BY (count(status) FILTER (WHERE status = '404 NOT FOUND') * 1.000) / (count(status) * 1.000) desc
-   LIMIT 10;
+   ORDER BY (count(status) FILTER (WHERE status = '404 NOT FOUND') * 1.000) / (count(status) * 1.000) desc;
