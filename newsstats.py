dd8df978b156fefd2895dcd8b0a0854635313990
obleft <bhhakes@gmail.com>
2017-07-29 12:15:33 -0500

    style: code spacing changes

--- newsstats.py
+++ newsstats.py
@@ -14,22 +14,21 @@ f = open(filename, "w+")
 # Header
 f.write("--- For Internal Purposes Only ---\n\n")
 f.write("* Web Traffic Log Analysis *\n\n")
-
 # Questions Section
 
 # Question 1
 f.write("1. What are the most popular three articles of all time?\n\n")
 ans1 = newsdata.artRank()
 for counter, ans in enumerate(ans1):
-    f.write("{}. {} -- {:,} pageviews\n"\
-        .format(counter + 1, ans1[counter][0], ans1[counter][1]))
+    f.write("{}. {} -- {:,} pageviews\n"
+            .format(counter + 1, ans1[counter][0], ans1[counter][1]))
 
 # Question 2
 f.write("\n\n2. Who are the most popular article authors of all time?\n\n")
 ans2 = newsdata.authRank()
 for counter, ans in enumerate(ans2):
-    f.write("{}. {} -- {:,} pageviews\n"\
-        .format(counter + 1, ans2[counter][0], ans2[counter][1]))
+    f.write("{}. {} -- {:,} pageviews\n"
+            .format(counter + 1, ans2[counter][0], ans2[counter][1]))
 
 # Question 3
 f.write("\n\n")
@@ -38,5 +37,5 @@ ans3 = newsdata.onePercentErrorDay()
 for counter, ans in enumerate(ans3):
     if ans3[counter][1] < 0.02:
         break
-    f.write("{}. {:%B %d, %Y} -- {:,.2f} percent error rate\n"\
-        .format(counter + 1, ans3[counter][0], ans3[counter][1] * 100))
+    f.write("{}. {:%B %d, %Y} -- {:,.2f} percent error rate\n"
+            .format(counter + 1, ans3[counter][0], ans3[counter][1] * 100))
